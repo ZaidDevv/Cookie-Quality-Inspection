5329bc47449c4f4aec892dbaf7884afee762a4dc
Zaid.H <53235358+ZaidDevv@users.noreply.github.com>
2020-12-07 21:40:34 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,16 +9,15 @@
 using namespace cv;
 using namespace std;
 
-vector<Vec3f> circles;
 //void avgColor(Vec3i c, Mat src, float&, float&, float&);
 
-int main(int argc, char** argv)
+int main()
 {
-	float valueGoodCaseAVg = 0, satGoodCaseAVg = 0, hueGoodCaseAVg = 0;
+	vector<Vec3f> circles;
 	Scalar myMean;
-	const char* filename = argc >= 2 ? argv[1] : "C:\\Users\\Z\\Downloads\\imgproc\\pic.png";
+	const char* filename = "C:\\Users\\Z\\Downloads\\imgproc\\pic.png";
 	// Loads an image
-	Mat src = imread(samples::findFile(filename), IMREAD_COLOR);
+	Mat src = imread(filename, IMREAD_COLOR);
 	// Check if image is loaded fine
 	if (src.empty()) {
 		printf(" Error opening image\n");
@@ -30,11 +29,10 @@ int main(int argc, char** argv)
 	Mat grayRef;
 	cvtColor(src, gray, COLOR_BGR2GRAY);
 	medianBlur(gray, gray, 5);
-	vector<Vec3f> circles;
 
 	HoughCircles(gray, circles, HOUGH_GRADIENT, 1,
 		gray.rows / 16,  // change this value to detect circles with different distances to each other
-		50, 23, 35, 70 // change the last two parameters
+		50, 23, 36, 70 // change the last two parameters
 	);
 	Mat myRef = imread("C:\\Users\\Z\\Downloads\\imgproc\\myref.png");
 
@@ -54,16 +52,12 @@ int main(int argc, char** argv)
 
 	Mat roiRef = myRef(Range(c1[1] - c1[2], c1[1] + c1[2] + 1), Range(c1[0] - c1[2], c1[0] + c1[2] + 1));
 
-	Mat1b masky(roiRef.rows, roiRef.cols);
-
-	Scalar myMeanRef = cv::mean(roiRef);
+	Scalar myMeanRef = mean(roiRef);
 
 	for (int i = 0; i < circles.size(); i++)
 	{
-		float valAvg = 0, satAvg = 0, hueAvg = 0;
 
 		Vec3i c = circles[i];
-		Vec3i fixedCircle = circles[0];
 		Point center = Point(c[0], c[1]);
 		// circle center
 		circle(src, center, 1, Scalar(0, 100, 100), 3, LINE_AA);
@@ -73,31 +67,19 @@ int main(int argc, char** argv)
 		//avgColor(c, src, valAvg, hueAvg, satAvg);
 
 		Mat roi = src(Range(abs(c[1] - c[2]), abs(c[1] + c[2] + 1)), Range(abs(c[0] - c[2]),abs(c[0] + c[2] + 1)));
-		Mat1b mask(roi.rows, roi.cols);
 
 		myMean = mean(roi);
-
 
 		float avgClr = ((myMean[0] + myMean[1] + myMean[2])/3);
 
 
 		cout << "avgColor " << avgClr << endl;
 
-		cout << "Blue AVERAGE DIFFERENCE: " << myMean[0]<< endl;
-
-		cout << "GREEN AVERAGE DIFFERENCE: " << myMean[1]<< endl;
-
-		cout << "RED AVERAGE DIFFERENCE: " << myMean[2]<< endl;
-
-
-		//cout << "RBlue AVERAGE DIFFERENCE: " << abs(myMeanRef[0]) << endl;
-
-		//cout << "RGREEN AVERAGE DIFFERENCE: " << abs(myMeanRef[1]) << endl;
-
-		//cout << "RRED AVERAGE DIFFERENCE: " << abs(myMeanRef[2]) << endl;
 
 		if (avgClr < 103) {
+			circle(src, center, 1, Scalar(0, 100, 100), 3, LINE_AA);
 			circle(src, center, radius, Scalar(255, 0, 0), 3, LINE_AA);
+
 		}
 
 		else {
@@ -110,7 +92,7 @@ int main(int argc, char** argv)
 	resize(src, outImg, cv::Size(), 0.50, 0.50);
 
 	imshow("Detected circles", outImg);
-	imshow("Detected Reference", myRef);
+	imshow("Reference", myRef);
 	waitKey();
 
 	return EXIT_SUCCESS;
